a632fded92c7e084a98709d89a2dbd311d2c3934
lidong <yogurt1270@163.com>
2017-07-02 21:46:05 +0800

    add test.py

--- runtest.py
+++ runtest.py
@@ -9,14 +9,6 @@ discover = unittest.defaultTestLoader.discover(test_dir, pattern='test*.py')
 
 
 if __name__ == "__name__":
-    '''
-        # 构造测试集
-    suite = unittest.TestSuite()
-    suite.addTest(TestAdd("test_add"))
-    suite.addTest(TestAdd("test_add2"))
-    suite.addTest(TestSub("test_sub"))
-    suite.addTest(TestSub("test_sub2"))
-    '''
 
     # 执行测试用例
     runner = unittest.TextTestRunner()
