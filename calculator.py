50b4e22456c72cb1fd92e08f3e5e0b483ebc1dd5
lidong <yogurt1270@163.com>
2017-07-02 15:17:43 +0800

    update calculator.py, test.py

--- calculator.py
+++ calculator.py
@@ -9,3 +9,7 @@ class Count:
     # 计算加法
     def add(self):
         return self.a + self.b
+
+    # 计算减法
+    def sub(self):
+        return self.a - self.b
--- test.py
+++ test.py
@@ -4,10 +4,9 @@ from calculator import Count
 import unittest
 
 
-class TestCount(unittest.TestCase):
-
+class TestAdd(unittest.TestCase):
     def setUp(self):
-        print "test start!"
+        print 'test add start!'
 
     def test_add(self):
         j = Count(2, 3)
@@ -18,14 +17,33 @@ class TestCount(unittest.TestCase):
         self.assertEqual(j.add(), 117)
 
     def tearDown(self):
-        print "test end!"
+        print 'test add end!'
+
+
+class TestSub(unittest.TestCase):
+    def setUp(self):
+        print "test sub start!"
+
+    def test_sub(self):
+        j = Count(2, 3)
+        self.assertEqual(j.sub(), -1)
+
+    def test_sub2(self):
+        j = Count(71, 46)
+        self.assertEqual(j.sub(), 25)
+
+    def tearDown(self):
+        print "test sub end!"
+
 
 if __name__ == "__name__":
-
     # 构造测试集
     suite = unittest.TestSuite()
-    suite.addTest(TestCount("test_add2"))
+    suite.addTest(TestAdd("test_add"))
+    suite.addTest(TestAdd("test_add2"))
+    suite.addTest(TestSub("test_sub"))
+    suite.addTest(TestSub("test_sub2"))
 
-    #执行测试用例
+    # 执行测试用例
     runner = unittest.TextTestRunner()
     runner.run(suite)
