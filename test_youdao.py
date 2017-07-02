2509a097747e853a93996ffd3459b37d27d86502
lidong <yogurt1270@163.com>
2017-07-02 21:47:16 +0800

    add test.py

---
+++ test_youdao.py
@@ -0,0 +1,30 @@
+# coding:utf-8
+
+from selenium import webdriver
+import unittest
+import time
+
+
+class MyTest(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
+        self.driver.implicitly_wait(10)
+        self.base_url = "http://www.youdao.com"
+
+    def test_youdao(self):
+        driver = self.driver
+        driver.get(self.base_url + '/')
+        driver.find_element_by_id("query").clear()
+        driver.find_element_by_id("query").send_keys("webdriver")
+        driver.find_element_by_id("qb").click()
+        time.sleep(2)
+        title = driver.title
+        self.assertEqual(title, "webdriver - 有道搜索")
+
+    def tearDown(self):
+        self.driver.quit()
+
+
+if __name__ == "__name__":
+    unittest.main()
